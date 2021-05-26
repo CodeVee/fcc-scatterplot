d542366644178e021e1b99409538165361b2dd49
Victor Onyebuchi <victorchike247@gmail.com>
2021-05-26 13:34:03 +0100

    added process function

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 const endpoint = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json';
 const width = 800;
 const height = 400;
+
+const processData = async () => {
+    const res = await fetch(endpoint);
+    const data = await res.json();
+
+}
+
+processData();
